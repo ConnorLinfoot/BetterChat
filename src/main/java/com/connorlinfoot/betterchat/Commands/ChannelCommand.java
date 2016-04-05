5152fbc111621d66f368263c41d82962beb1fdf6
Connor Linfoot <connorlinfoot@me.com>
2016-04-05 08:54:34 +0100

    Changed channel command to use a switch

--- src/main/java/com/connorlinfoot/betterchat/Commands/ChannelCommand.java
+++ src/main/java/com/connorlinfoot/betterchat/Commands/ChannelCommand.java
@@ -22,124 +22,122 @@ public class ChannelCommand implements CommandExecutor {
 		}
 
 		boolean isAdmin = player.hasPermission("betterchat.admin");
-		if (args[0].equalsIgnoreCase("help")) {
-			player.sendMessage(ChatColor.AQUA + "/channel - View what channel you are currently in");
-			player.sendMessage(ChatColor.AQUA + "/channel <name> - Switch channels");
-			if (isAdmin) player.sendMessage(ChatColor.AQUA + "/channel create <name> - Create a channel");
-			if (isAdmin) player.sendMessage(ChatColor.AQUA + "/channel delete <name> - Delete a channel");
-//            if( isAdmin ) player.sendMessage(ChatColor.AQUA + "/channel edit <name> <option> <value> - Edit a channel option");
-			if (isAdmin) player.sendMessage(ChatColor.AQUA + "/channel info <name> - View information on a channel");
-			return true;
+		switch (args[0].toLowerCase()) {
+			default:
+				StringBuilder stringBuilder = new StringBuilder();
+				for (String value : args) {
+					stringBuilder.append(value).append(" ");
+				}
+
+				String channel = stringBuilder.toString();
+				channel = channel.replaceAll("\\s+$", "");
+				if (!ChannelHandler.channelExists(channel)) {
+					player.sendMessage(ChatColor.RED + "The channel \"" + channel + "\" could not be found");
+					return false;
+				}
+
+				if (ChannelHandler.channelUsesPerms(channel) && !player.hasPermission("betterchat.channel." + channel) && !player.hasPermission("betterchat.all")) {
+					player.sendMessage(ChatColor.RED + "You don't have permission to join this channel");
+					return false;
+				}
+
+				ChannelHandler.setPlayerChannel(player, channel, true);
+				player.sendMessage(ChatColor.GREEN + "You are now talking in the channel \"" + channel + "\"");
+				break;
+			case "help":
+				player.sendMessage(ChatColor.AQUA + "/channel - View what channel you are currently in");
+				player.sendMessage(ChatColor.AQUA + "/channel <name> - Switch channels");
+				if (isAdmin) player.sendMessage(ChatColor.AQUA + "/channel create <name> - Create a channel");
+				if (isAdmin) player.sendMessage(ChatColor.AQUA + "/channel delete <name> - Delete a channel");
+//            	if( isAdmin ) player.sendMessage(ChatColor.AQUA + "/channel edit <name> <option> <value> - Edit a channel option");
+				if (isAdmin)
+					player.sendMessage(ChatColor.AQUA + "/channel info <name> - View information on a channel");
+				break;
+			case "create":
+				if (!isAdmin) {
+					player.sendMessage(ChatColor.RED + "You do not have permission to run this");
+					return false;
+				}
+
+				if (args.length < 2) {
+					player.sendMessage(ChatColor.RED + "Correct Usage: /channel create <name>");
+					return false;
+				}
+
+				stringBuilder = new StringBuilder();
+				for (String value : args) {
+					stringBuilder.append(value).append(" ");
+				}
+
+				channel = stringBuilder.toString();
+				channel = channel.replaceFirst(args[0] + " ", "");
+				channel = channel.replaceAll("\\s+$", "");
+
+				if (ChannelHandler.channelExists(channel)) {
+					player.sendMessage(ChatColor.RED + "That channel already exists");
+					return false;
+				}
+
+				ChannelHandler.createChannel(channel);
+				player.sendMessage(ChatColor.GREEN + "The channel \"" + channel + "\" has been created");
+				break;
+			case "delete":
+				if (!isAdmin) {
+					player.sendMessage(ChatColor.RED + "You do not have permission to run this");
+					return false;
+				}
+
+				if (args.length < 2) {
+					player.sendMessage(ChatColor.RED + "Correct Usage: /channel delete <name>");
+					return false;
+				}
+
+				stringBuilder = new StringBuilder();
+				for (String value : args) {
+					stringBuilder.append(value).append(" ");
+				}
+
+				channel = stringBuilder.toString();
+				channel = channel.replaceFirst(args[0] + " ", "");
+				channel = channel.replaceAll("\\s+$", "");
+
+				if (!ChannelHandler.channelExists(channel)) {
+					player.sendMessage(ChatColor.RED + "That channel doesn't exist");
+					return false;
+				}
+
+				ChannelHandler.deleteChannel(channel);
+				player.sendMessage(ChatColor.GREEN + "The channel \"" + channel + "\" has been deleted");
+				break;
+			case "info":
+				if (!isAdmin) {
+					player.sendMessage(ChatColor.RED + "You do not have permission to run this");
+					return false;
+				}
+
+				if (args.length < 2) {
+					player.sendMessage(ChatColor.RED + "Correct Usage: /channel info <name>");
+					return false;
+				}
+
+				stringBuilder = new StringBuilder();
+				for (String value : args) {
+					stringBuilder.append(value).append(" ");
+				}
+
+				channel = stringBuilder.toString();
+				channel = channel.replaceFirst(args[0] + " " + args[1] + " ", "");
+				channel = channel.replaceAll("\\s+$", "");
+
+				if (!ChannelHandler.channelExists(channel)) {
+					player.sendMessage(ChatColor.RED + "That channel doesn't exist");
+					return false;
+				}
+
+				ChannelHandler.channelInfo(channel, player);
+				break;
 		}
-
-		if (args[0].equalsIgnoreCase("create")) {
-			if (!isAdmin) {
-				player.sendMessage(ChatColor.RED + "You do not have permission to run this");
-				return false;
-			}
-
-			if (args.length < 2) {
-				player.sendMessage(ChatColor.RED + "Correct Usage: /channel create <name>");
-				return false;
-			}
-
-			StringBuilder stringBuilder = new StringBuilder();
-			for (String value : args) {
-				stringBuilder.append(value).append(" ");
-			}
-
-			String channel = stringBuilder.toString();
-			channel = channel.replaceFirst(args[0] + " ", "");
-			channel = channel.replaceAll("\\s+$", "");
-
-			if (ChannelHandler.channelExists(channel)) {
-				player.sendMessage(ChatColor.RED + "That channel already exists");
-				return false;
-			}
-
-			ChannelHandler.createChannel(channel);
-			player.sendMessage(ChatColor.GREEN + "The channel \"" + channel + "\" has been created");
-			return true;
-		}
-
-		if (args[0].equalsIgnoreCase("delete")) {
-			if (!isAdmin) {
-				player.sendMessage(ChatColor.RED + "You do not have permission to run this");
-				return false;
-			}
-
-			if (args.length < 2) {
-				player.sendMessage(ChatColor.RED + "Correct Usage: /channel delete <name>");
-				return false;
-			}
-
-			StringBuilder stringBuilder = new StringBuilder();
-			for (String value : args) {
-				stringBuilder.append(value).append(" ");
-			}
-
-			String channel = stringBuilder.toString();
-			channel = channel.replaceFirst(args[0] + " ", "");
-			channel = channel.replaceAll("\\s+$", "");
-
-			if (!ChannelHandler.channelExists(channel)) {
-				player.sendMessage(ChatColor.RED + "That channel doesn't exist");
-				return false;
-			}
-
-			ChannelHandler.deleteChannel(channel);
-			player.sendMessage(ChatColor.GREEN + "The channel \"" + channel + "\" has been deleted");
-			return true;
-		}
-
-		if (args[0].equalsIgnoreCase("info")) {
-			if (!isAdmin) {
-				player.sendMessage(ChatColor.RED + "You do not have permission to run this");
-				return false;
-			}
-
-			if (args.length < 2) {
-				player.sendMessage(ChatColor.RED + "Correct Usage: /channel info <name>");
-				return false;
-			}
-
-			StringBuilder stringBuilder = new StringBuilder();
-			for (String value : args) {
-				stringBuilder.append(value).append(" ");
-			}
-
-			String channel = stringBuilder.toString();
-			channel = channel.replaceFirst(args[0] + " " + args[1] + " ", "");
-			channel = channel.replaceAll("\\s+$", "");
-
-			if (!ChannelHandler.channelExists(channel)) {
-				player.sendMessage(ChatColor.RED + "That channel doesn't exist");
-				return false;
-			}
-
-			ChannelHandler.channelInfo(channel, player);
-		}
-
-		StringBuilder stringBuilder = new StringBuilder();
-		for (String value : args) {
-			stringBuilder.append(value).append(" ");
-		}
-
-		String channel = stringBuilder.toString();
-		channel = channel.replaceAll("\\s+$", "");
-		if (!ChannelHandler.channelExists(channel)) {
-			player.sendMessage(ChatColor.RED + "The channel \"" + channel + "\" could not be found");
-			return false;
-		}
-
-		if (ChannelHandler.channelUsesPerms(channel) && !player.hasPermission("betterchat.channel." + channel) && !player.hasPermission("betterchat.all")) {
-			player.sendMessage(ChatColor.RED + "You don't have permission to join this channel");
-			return false;
-		}
-
-		ChannelHandler.setPlayerChannel(player, channel, true);
-		player.sendMessage(ChatColor.GREEN + "You are now talking in the channel \"" + channel + "\"");
-		return false;
+		return true;
 	}
 
 }
